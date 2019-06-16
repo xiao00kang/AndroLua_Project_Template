6d7ee1180b9afa3b43af771ec3685cbb61dd880c
1753520469 <1753520469@qq.com>
2019-06-16 23:29:37 +0800

    Initial commit

---
+++ app/src/main/assets/init.lua
@@ -0,0 +1,12 @@
+appname="AndroLua Project Demo"
+appver="1.0"
+app_key=""
+app_channel=""
+developer="MoXiaoDi"
+description="AndroLua Project Demo"
+debugmode=true
+theme="Theme_DeviceDefault_Light_DarkActionBar"
+packagename="com.androlua"
+user_permission={
+  "WRITE_EXTERNAL_STORAGE",
+}
---
+++ app/src/main/assets/main.lua
@@ -0,0 +1,17 @@
+require "import"
+import "console"
+import "android.app.*"
+import "android.os.*"
+import "android.widget.*"
+import "android.view.*"
+import "com.androlua.*"
+import "java.io.*"
+import "android.text.method.*"
+import "android.net.*"
+import "android.content.*"
+import "android.graphics.drawable.*"
+
+activity.setContentView(loadlayout"layout.main")
+
+
+
