6023de2e7f3a6241e90234ff53c73acbc54b49e1
墨小迪 <1753520469@qq.com>
2019-06-16 23:48:21 +0800

    Update main.lua

--- app/src/main/assets/main.lua
+++ app/src/main/assets/main.lua
@@ -1,15 +1,8 @@
 require "import"
-import "console"
-import "android.app.*"
 import "android.os.*"
-import "android.widget.*"
+import "android.app.*"
 import "android.view.*"
-import "com.androlua.*"
-import "java.io.*"
-import "android.text.method.*"
-import "android.net.*"
-import "android.content.*"
-import "android.graphics.drawable.*"
+import "android.widget.*"
 
 activity.setContentView(loadlayout"layout.main")
 
